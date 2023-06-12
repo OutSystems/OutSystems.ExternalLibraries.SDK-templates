edef38840ced282cb4cfdf405ccdac2de9e84a4b
Jonathan Algar <93204286+jonathanalgar@users.noreply.github.com>
2023-06-12 05:35:04 +0100

    Edit and expand comments

--- templates/soap/ConsumeSOAPExample/Structures/Numbers.cs
+++ templates/soap/ConsumeSOAPExample/Structures/Numbers.cs
@@ -1,14 +1,25 @@
 using OutSystems.ExternalLibraries.SDK;
 
 namespace ConsumeSOAPExample.Structures {
-    // It's exposed as a structure to your ODC apps and libraries.
+    /// <summary>
+    /// The Numbers struct contains two numbers (NumberA and NumberB) for 
+    /// mathematical operations. It's exposed as a structure to your ODC apps
+    /// and libraries.
+    /// </summary>
     [OSStructure(Description = "Number A & B")]
     public struct Numbers {
-        // Gets Number A & B to do the sum.
         [OSStructureField(DataType = OSDataType.Integer, Description = "Number A.", IsMandatory = true)]
+        /// <summary>
+        /// Gets Number A, which is one of the operands for mathematical
+        /// operations.
+        /// </summary>
         public int NumberA;
 
         [OSStructureField(DataType = OSDataType.Integer, Description = "Number B.", IsMandatory = true)]
+        /// <summary>
+        /// Gets Number B, which is one of the operands for mathematical
+        /// operations.
+        /// </summary>
         public int NumberB;
     }
 }
