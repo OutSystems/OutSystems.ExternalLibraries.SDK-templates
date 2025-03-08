d2f56ad598b13856d00d0c5704cabdfbd033019a
Diogo Paulo (dpa) <diogo.paulo@outsystems.com>
2025-03-08 14:59:40 +0000

    mTLS Service and Client projects with code

---
+++ templates/mtls/client/mTLSClientLib/ServiceReference/ExampleClientBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace mTLSclient {
+    internal class ExampleClientBase {
+        private static readonly string ClientCertificatePath = @"./Certificates/mTLSclient.pfx";
+
+        private static bool ValidateServerCertificate(HttpRequestMessage request, X509Certificate2? certificate, X509Chain? chain, SslPolicyErrors errors) {
+            // Perform custom server certificate validation if required
+            // Return true if the certificate is trusted, false otherwise
+            return true;
+        }
+
+        protected Task<HttpClient> CreateHttpClientAsync(CancellationToken cancellationToken) {
+            // Load the client certificate
+            if(!File.Exists(ClientCertificatePath)) {
+                throw new FileNotFoundException($"Could not find the file {ClientCertificatePath}");
+            }
+            var clientCertificate = new X509Certificate2(ClientCertificatePath, "TestSecret!");
+
+            // Create an HttpClient with MTLS authentication
+            var httpClient = new HttpClient(new HttpClientHandler {
+                ClientCertificateOptions = ClientCertificateOption.Manual,
+                SslProtocols = SslProtocols.Tls12,
+                ServerCertificateCustomValidationCallback = ValidateServerCertificate,
+                ClientCertificates = { clientCertificate }
+            });
+            return Task.FromResult(httpClient);
+        }
+
+    }
+}
---
+++ templates/mtls/client/mTLSClientLib/mTLSClientExample.cs
@@ -0,0 +1,19 @@
+﻿using mTLSclient;
+using OutSystems.ExternalLibraries.SDK;
+
+namespace mTLSClientLib;
+
+[OSInterface(Description = "Consume a mTLS web service in OutSystems Developer Cloud (ODC).", Name = "mTLS_Client")]
+public interface ImTLSClientExample {
+    [OSAction(Description = "Get Weather Forecast for the next 5 days.", ReturnName = "WeatherForecast")]
+    ICollection<WeatherForecast> GetWeatherForecast(
+        [OSParameter(Description = "Service Base Url.", DataType = OSDataType.Text)] 
+        string baseUrl);
+}
+
+public class mTLSClientExample : ImTLSClientExample {
+    public ICollection<WeatherForecast> GetWeatherForecast(string baseUrl) {
+        var client = new ExampleClient(baseUrl);
+        return client.GetWeatherForecastAsync().GetAwaiter().GetResult();
+    }
+}
