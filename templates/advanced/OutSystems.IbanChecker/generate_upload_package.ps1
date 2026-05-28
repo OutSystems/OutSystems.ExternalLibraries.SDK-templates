6513ff19a57052051b680b2273e587767cf24a3d
José Santos <jose.dos.santos@outsystems.com>
2026-05-28 14:29:44 +0100

    RTAFB-8909: rename .net 10 zip

--- templates/advanced/OutSystems.IbanChecker/generate_upload_package.ps1
+++ templates/advanced/OutSystems.IbanChecker/generate_upload_package.ps1
@@ -2,5 +2,6 @@ Set-ExecutionPolicy -Scope CurrentUser Unrestricted
 
 foreach ($tfm in @("net8.0", "net10.0")) {
     dotnet publish -c Release -r linux-x64 --self-contained false -f $tfm
-    Compress-Archive -Path ".\bin\Release\$tfm\linux-x64\publish\*" -DestinationPath "ExternalLibrary_$tfm.zip" -Force
+    $zipName = if ($tfm -eq "net10.0") { "ExternalLibrary.zip" } else { "ExternalLibrary_$tfm.zip" }
+    Compress-Archive -Path ".\bin\Release\$tfm\linux-x64\publish\*" -DestinationPath $zipName -Force
 }
